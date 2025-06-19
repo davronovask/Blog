fba35a24037c51ae7f27dedccec5cf9d9f04719d
davronovask <166344424+davronovask@users.noreply.github.com>
2025-06-19 14:29:19 +0600

    в админку пост

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,10 @@
+# admin.py
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'created_at')
+    search_fields = ('title', 'content', 'author__email')
+    list_filter = ('created_at', 'profession')
+    readonly_fields = ('created_at',)
