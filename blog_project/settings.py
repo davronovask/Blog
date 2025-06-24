06ef23069177438776810869edfee42220fe1a32
davronovask <166344424+davronovask@users.noreply.github.com>
2025-06-24 13:45:13 +0600

    закоментировала доступ для корс всем доменам и сделала выборочные

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -151,12 +151,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-#     "https://effortless-donut-b16125.netlify.app",
-#
-# ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://effortless-donut-b16125.netlify.app",
+    "https://mellow-klepon-f73b10.netlify.app",
+]
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
